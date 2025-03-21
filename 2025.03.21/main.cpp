263afb4a3bcf469f35df9134fdfa4814c016c186
CaraSun <1760234558@qq.com>
2025-03-21 19:33:16 +0800

    第一次提交

--- 2025.03.21/main.cpp
+++ 2025.03.21/main.cpp
@@ -1,9 +1,9 @@
-
+#include <stdio.h>
 
 void fun(int array[3]) {
     printf("fun:sizeof(array) = %d\n", sizeof(array));
 }
-#include <stdio.h>
+
 int main() {
     int array[3] = {1, 2, 3};
     long int a = 6789012345L;
