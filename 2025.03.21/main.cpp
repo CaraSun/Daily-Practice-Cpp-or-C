007ed0be5f0398dfefed0ce9bc07efed6334715f
CaraSun <46025747+CaraSun@users.noreply.github.com>
2025-03-21 11:06:34 +0800

    第一次提交

---
+++ 2025.03.21/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void fun(int array[3]){
+    printf("fun:sizeof(array) = %d\n",sizeof(array));
+}
+
+int main(){
+    int array[3] = {1,2,3};
+    long int a = 6789012345L;
+    printf("main:sizeof(array) = %d\n",sizeof(array));
+    printf("sizeof(long int) = %d\n",sizeof(a));
+    fun(array);
+    return 0;
+}
